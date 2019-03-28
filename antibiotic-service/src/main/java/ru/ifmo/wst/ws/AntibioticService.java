e97b84074daff53c465d95268b3aa13dae05e996
Alexey Mishenev <mishenevalex@gmail.com>
2019-03-28 04:31:59 +0300

    Added Service module

---
+++ antibiotic-service/src/main/java/ru/ifmo/wst/ws/AntibioticService.java
@@ -0,0 +1,55 @@
+package ru.ifmo.wst.ws;
+
+import java.util.List;
+import javax.inject.Inject;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import ru.ifmo.wst.dao.AntibioticsDAO;
+import ru.ifmo.wst.entity.Antibiotics;
+
+@WebService
+@NoArgsConstructor
+@AllArgsConstructor
+public class AntibioticService {
+
+  @Inject
+  private AntibioticsDAO antibioticsDAO;
+
+  @WebMethod
+  @SneakyThrows
+  public List<Antibiotics> findAll() {
+    return antibioticsDAO.findAll();
+  }
+
+  @WebMethod
+  @SneakyThrows
+  public List<String> findAllAntibiotics() {
+    return antibioticsDAO.findAllAntibiotics();
+  }
+
+  @WebMethod
+  @SneakyThrows
+  public String findDosage(
+      @WebParam(name = "name") String name,
+      @WebParam(name = "method") String method,
+      @WebParam(name = "skf") Integer skf) {
+    return antibioticsDAO.findDosage(name, method, skf);
+  }
+
+  @WebMethod
+  @SneakyThrows
+  public List<Antibiotics> filter(
+      @WebParam(name = "id") Long id,
+      @WebParam(name = "name") String name,
+      @WebParam(name = "method") String method,
+      @WebParam(name = "from") Integer from,
+      @WebParam(name = "to") Integer to,
+      @WebParam(name = "dosage") String dosage,
+      @WebParam(name = "additional") String additional) {
+    return antibioticsDAO.filter(id, name, method, to, from, dosage, additional);
+  }
+}
