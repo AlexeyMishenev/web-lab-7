d4bd8c6b1492b4680b9643476540c78c3f469f31
Alexey Mishenev <mishenevalex@gmail.com>
2019-05-25 19:18:34 +0300

    Move all cases to its own methods and add some new methods.
    Partially applied commits from lab 4:
    - 40d781ac6c3da189add388c5f97734e5c31bf616
    - bcd440dcbfb1848853a53f627e9b9963810a9e4d
    - ed1ac0fcffa4bae8ba0c19c7f33b5494f437e6f1

--- client/src/main/java/ru/ifmo/wst/client/Client.java
+++ client/src/main/java/ru/ifmo/wst/client/Client.java
@@ -1,21 +1,27 @@
 package ru.ifmo.wst.client;
 
 
+import static java.text.MessageFormat.format;
 import static ru.ifmo.wst.client.Utils.readInt;
 import static ru.ifmo.wst.client.Utils.readLong;
+import static ru.ifmo.wst.client.Utils.readNotNullLong;
+import static ru.ifmo.wst.client.Utils.readNotNullString;
 import static ru.ifmo.wst.client.Utils.readString;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Objects;
+import java.util.List;
 
 public class Client {
+  private static AntibioticService service;
+
   public static void main(String[] args) throws IOException {
     URL url = new URL(args[0]);
     AntibioticServiceService antibioticService = new AntibioticServiceService(url);
-    AntibioticService antibioticPort = antibioticService.getAntibioticServicePort();
+
+    service = antibioticService.getAntibioticServicePort();
 
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     int curState = 0;
@@ -35,9 +41,8 @@ public class Client {
           curState = readState(curState, reader);
           break;
         case 1:
-          System.out.println("Найдено:");
           try {
-            antibioticPort.findAllAntibiotics().forEach(System.out::println);
+            findAll();
           } catch (AntibioticServiceException e) {
             System.err.println(e.getFaultInfo().getMessage());
           } finally {
@@ -45,9 +50,8 @@ public class Client {
           }
           break;
         case 2:
-          System.out.println("Найдено:");
           try {
-            antibioticPort.findAll().stream().map(Object::toString).forEach(System.out::println);
+            findAllDosages();
           } catch (AntibioticServiceException e) {
             System.err.println(e.getFaultInfo().getMessage());
           } finally {
@@ -55,22 +59,8 @@ public class Client {
           }
           break;
         case 3:
-          System.out.println("\nЗаполните все поля");
-          String findName;
-          do {
-            System.out.println("Название:");
-            findName = readString(reader);
-          } while (findName == null);
-
-          System.out.println("Метод введения:");
-          String findMethod = readString(reader);
-
-          System.out.println("СКФ (мл/мин):");
-          Integer skf = readInt(reader);
-
-          System.out.println("Найдено:");
           try {
-            System.out.println(antibioticPort.findDosage(findName, findMethod, skf));
+            getDosageBySKF(reader);
           } catch (AntibioticServiceException e) {
             System.err.println(e.getFaultInfo().getMessage());
           } finally {
@@ -78,21 +68,8 @@ public class Client {
           }
           break;
         case 4:
-          System.out.println("\nЧтобы не применять фильтр, оставьте значение пустым");
-          System.out.println("id:");
-          Long id = readLong(reader);
-          System.out.println("Название:");
-          String name = readString(reader);
-          System.out.println("Метод введения:");
-          String method = readString(reader);
-          System.out.println("СКФ от:");
-          Integer from = readInt(reader);
-          System.out.println("СКФ до:");
-          Integer to = readInt(reader);
-          System.out.println("Найдено:");
           try {
-            antibioticPort.filter(id, name, method, from, to, null, null).stream()
-                .map(Objects::toString).forEach(System.out::println);
+            filter(reader);
           } catch (AntibioticServiceException e) {
             System.err.println(e.getFaultInfo().getMessage());
           } finally {
@@ -100,38 +77,8 @@ public class Client {
           }
           break;
         case 5:
-          System.out.println("\nЗаполните поля (* - обязательные)");
-          String createName;
-          do {
-            System.out.println("* Название:");
-            createName = readString(reader);
-          } while (createName == null);
-
-          System.out.println("Метод введения:");
-          String createMethod = readString(reader);
-
-          System.out.println("СКФ От (0 если пустое):");
-          Integer createFrom = readInt(reader, 0);
-          System.out.println("СКФ До (1000 если пустое):");
-          Integer createTo = readInt(reader, 1000);
-
-          String createDosage;
-          do {
-            System.out.println("* Дозировка:");
-            createDosage = readString(reader);
-          } while (createDosage == null);
-
-          System.out.println("Дополнительно:");
-          String createAdditional = readString(reader);
-
-          if (createAdditional != null && !createDosage.endsWith("*")) {
-            createDosage += "*";
-          }
-
           try {
-            long createdId = antibioticPort.create(createName, createMethod,
-                createFrom, createTo, createDosage, createAdditional);
-            System.out.println("ID новой записи: " + createdId);
+            create(reader);
           } catch (AntibioticServiceException e) {
             System.err.println(e.getFaultInfo().getMessage());
           } finally {
@@ -139,33 +86,8 @@ public class Client {
           }
           break;
         case 6:
-          Long updateId;
-          do {
-            System.out.println("id изменяемой записи (0 для отмены операции):");
-            updateId = readLong(reader);
-          } while (updateId == null);
-
-          if (updateId == 0L) {
-            curState = 0;
-            break;
-          }
-
-          System.out.println("* Название:");
-          String updateName = readString(reader);
-          System.out.println("Метод введения:");
-          String updateMethod = readString(reader);
-          System.out.println("СКФ От (0 если пустое):");
-          Integer updateFrom = readInt(reader, 0);
-          System.out.println("СКФ До (1000 если пустое):");
-          Integer updateTo = readInt(reader, 1000);
-          System.out.println("* Дозировка:");
-          String updateDosage = readString(reader);
-          System.out.println("Дополнительно:");
-          String updateAdditional = readString(reader);
           try {
-            long updateRes = antibioticPort.update(updateId,
-                updateName, updateMethod, updateFrom, updateTo, updateDosage, updateAdditional);
-            System.out.println("Изменено " + updateRes + " строк");
+            update(reader);
           } catch (AntibioticServiceException e) {
             System.err.println(e.getFaultInfo().getMessage());
           } finally {
@@ -173,18 +95,8 @@ public class Client {
           }
           break;
         case 7:
-          Long deleteId;
-          do {
-            System.out.println("id удаляемой записи (0 для отмены операции):");
-            deleteId = readLong(reader);
-          } while (deleteId == null);
-          if (deleteId == 0L) {
-            curState = 0;
-            break;
-          }
           try {
-            int deleteRes = antibioticPort.delete(deleteId);
-            System.out.println("Удалено " + deleteRes + " строк(а)");
+            delete(reader);
           } catch (AntibioticServiceException e) {
             System.err.println(e.getFaultInfo().getMessage());
           } finally {
@@ -199,6 +111,111 @@ public class Client {
       }
     }
   }
+
+  private static void findAll() throws AntibioticServiceException {
+    List<String> drugs = service.findAllAntibiotics();
+
+    if (drugs.size() == 0) {
+      System.out.println("Ничего не найдено");
+    } else {
+      System.out.println("Найдено:");
+      drugs.forEach(System.out::println);
+    }
+  }
+
+  private static void findAllDosages() throws AntibioticServiceException {
+    List<Antibiotics> drugs = service.findAll();
+
+    if (drugs.size() == 0) {
+      System.out.println("Ничего не найдено");
+    } else {
+      System.out.println("Найдено:");
+      drugs.stream().map(Antibiotics::toString).forEach(System.out::println);
+    }
+  }
+
+  private static void getDosageBySKF(BufferedReader reader) throws AntibioticServiceException {
+    System.out.println("\nЗаполните все поля");
+
+    String name = readNotNullString("Название:", reader);
+    String method = readString("Метод введения:", reader);
+    Integer skf = readInt("СКФ (мл/мин):", reader);
+
+    System.out.println("Найдено:");
+    System.out.println(service.findDosage(name, method, skf));
+  }
+
+  private static void filter(BufferedReader reader) throws AntibioticServiceException {
+    System.out.println("\nЧтобы не применять фильтр, оставьте значение пустым");
+
+    Long id = readLong("id:", reader);
+    String name = readString("Название:", reader);
+    String method = readString("Метод введения:", reader);
+    Integer from = readInt("СКФ от:", reader);
+    Integer to = readInt("СКФ до:", reader);
+    String dosage = readString("Дозировка:", reader);
+    String additional = readString("Дополнительно:", reader);
+
+    List<Antibiotics> drugs = service.filter(id, name, method, from, to, dosage, additional);
+
+    if (drugs.size() == 0) {
+      System.out.println("Ничего не найдено");
+    } else {
+      System.out.println("Найдено:");
+      drugs.stream().map(Antibiotics::toString).forEach(System.out::println);
+    }
+  }
+
+  private static void create(BufferedReader reader) throws AntibioticServiceException {
+    System.out.println("\nЗаполните поля (* - обязательные)");
+
+    String name = readNotNullString("* Название:", reader);
+    String method = readString("Метод введения:", reader);
+    Integer from = readInt("СКФ От (0 если пустое):", reader, 0);
+    Integer to = readInt("СКФ До (1000 если пустое):", reader, 1000);
+    String dosage = readNotNullString("* Дозировка:", reader);
+    String additional = readString("Дополнительно:", reader);
+
+    if (additional != null && !dosage.endsWith("*")) {
+      dosage += "*";
+    }
+
+    System.out.println(format("ID новой записи: {0}",
+        service.create(name, method, from, to, dosage, additional)));
+  }
+
+  private static void update(BufferedReader reader) throws AntibioticServiceException {
+    long id = readNotNullLong("id изменяемой записи (0 для отмены операции):", reader);
+
+    if (id == 0L) {
+      return;
+    }
+
+    String name = readString("Название:", reader);
+    String method = readString("Метод введения:", reader);
+    Integer from = readInt("СКФ От (0 если пустое):", reader, 0);
+    Integer to = readInt("СКФ До (1000 если пустое):", reader, 100);
+    String dosage = readString("Дозировка:", reader);
+    String additional = readString("Дополнительно:", reader);
+
+    if (additional != null && dosage != null && !dosage.endsWith("*")) {
+      dosage += "*";
+    }
+
+    System.out.println(format("Изменено {0} строк(а)",
+        service.update(id, name, method, from, to, dosage, additional)));
+  }
+
+  private static void delete(BufferedReader reader) throws AntibioticServiceException {
+    long id = readNotNullLong("id удаляемой записи (0 для отмены операции):", reader);
+
+    if (id == 0L) {
+      return;
+    }
+
+    System.out.println(format("Удалено {0} строк(а)", service.delete(id)));
+  }
+
 
   private static int readState(int cur, BufferedReader reader) {
     try {
--- client/src/main/java/ru/ifmo/wst/client/Utils.java
+++ client/src/main/java/ru/ifmo/wst/client/Utils.java
@@ -4,39 +4,106 @@ import java.io.BufferedReader;
 
 class Utils {
 
-  static String readString(BufferedReader reader) {
+  static String readString(BufferedReader reader, String defaultValue) {
     try {
       String trim = reader.readLine().trim();
       if (trim.isEmpty()) {
-        return null;
+        return defaultValue;
       }
       return trim;
     } catch (Exception e) {
-      return null;
+      return defaultValue;
     }
   }
 
-  static Long readLong(BufferedReader reader) {
+  static Long readLong(BufferedReader reader, Long defaultValue) {
     try {
       return Long.parseLong(reader.readLine());
     } catch (Exception e) {
-      return null;
+      return defaultValue;
     }
   }
 
-  static Integer readInt(BufferedReader reader) {
-    try {
-      return Integer.parseInt(reader.readLine());
-    } catch (Exception e) {
-      return null;
-    }
-  }
-
-  static Integer readInt(BufferedReader reader, int defaultValue) {
+  static Integer readInt(BufferedReader reader, Integer defaultValue) {
     try {
       return Integer.parseInt(reader.readLine());
     } catch (Exception e) {
       return defaultValue;
     }
   }
+
+  static String readString(BufferedReader reader) {
+    return readString(reader, null);
+  }
+
+  static Integer readInt(BufferedReader reader) {
+    return readInt(reader, null);
+  }
+
+  static Long readLong(BufferedReader reader) {
+    return readLong(reader, null);
+  }
+
+  static String readNotNullString(String header, BufferedReader reader) {
+    String value;
+    do {
+      System.out.println(header);
+      value = readString(reader);
+    } while (value == null);
+
+    return value;
+  }
+
+  static int readNotNullInt(String header, BufferedReader reader) {
+    Integer value;
+
+    do {
+      System.out.println(header);
+      value = readInt(reader);
+    } while (value == null);
+
+    return value;
+  }
+
+  static long readNotNullLong(String header, BufferedReader reader) {
+    Long value;
+
+    do {
+      System.out.println(header);
+      value = readLong(reader);
+    } while (value == null);
+
+    return value;
+  }
+
+  static String readString(String header, BufferedReader reader) {
+    System.out.println(header);
+    return readString(reader, null);
+  }
+
+  static Integer readInt(String header, BufferedReader reader) {
+    System.out.println(header);
+    return readInt(reader, null);
+  }
+
+  static Long readLong(String header, BufferedReader reader) {
+    System.out.println(header);
+    return readLong(reader, null);
+  }
+
+  static String readString(String header, BufferedReader reader, String defaultValue) {
+    System.out.println(header);
+    return readString(reader, defaultValue);
+  }
+
+  static Integer readInt(String header, BufferedReader reader, Integer defaultValue) {
+    System.out.println(header);
+    return readInt(reader, defaultValue);
+  }
+
+  static Long readLong(String header, BufferedReader reader, Long defaultValue) {
+    System.out.println(header);
+    return readLong(reader, defaultValue);
+  }
+
 }
